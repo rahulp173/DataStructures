dc6fd9c9e7a073c88c5ef22c6161bd8c7b16b1ec
Rahul Chouhan <rahulchouhan453@gmail.com>
2024-07-20 22:38:14 +0530

    Added bubble and selection Sort

---
+++ bubbleSort.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+int x[8] = {5,89,13,47,80,23,100,91};
+int i,j,k,temp;
+for(i=0;i<7;i++)
+{
+printf("%d\n", x[i]);
+}
+
+j=8-1;
+k;
+while(j>0)
+{
+k=0;
+while(k<j)
+{
+if(x[k]>x[k+1])
+{
+temp=x[k];
+x[k]=x[k+1];
+x[k+1]=temp;
+}
+k++;
+}
+j--;
+}
+
+printf("After Bubble Sort Algorithm\n");
+
+for(i=0;i<7;i++)
+{
+printf("%d\n", x[i]);
+}
+
+
+return 0;
+}
---
+++ selectionSort.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+int main()
+{
+int x[8] = {5,89,13,47,80,23,100,91};
+int i,selectedIndex,j,temp;
+for(i=0;i<7;i++)
+{
+printf("%d\n", x[i]);
+}
+
+while(selectedIndex<(8-1))
+{
+j=selectedIndex+1;
+while(j<=(8-1))
+{
+if(x[selectedIndex]>x[j])
+{
+temp=x[selectedIndex];
+x[selectedIndex]=x[j];
+x[j]=temp;
+}
+j++;
+}
+selectedIndex++;
+}
+
+printf("After Selection Sort Algorithm\n");
+
+for(i=0;i<7;i++)
+{
+printf("%d\n", x[i]);
+}
+
+
+return 0;
+}
